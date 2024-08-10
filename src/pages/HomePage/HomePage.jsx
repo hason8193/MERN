a7101e25954944316db909083790b6528e7976c5
hason <tranhasonpy@gmail.com>
2024-08-10 16:32:09 +0700

    add slider

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import TypeProduct from '../../components/TypeProduct/TypeProduct'
 import { WrapperTypeProduct } from './style'
+import SliderComponent from '../../components/SliderComponent/SliderComponent'
+import slider1 from '../../assets/images/slider1.webp'
+import slider2 from '../../assets/images/slider2.webp'
+import slider3 from '../../assets/images/slider3.webp'
+import slider4 from '../../assets/images/slider4.webp'
 const HomePage = () => {
   const arr = ['TV','Laptop','Phone','Tablet','Watch','Headphone','Camera','Printer','Speaker','Monitor']
   return (
@@ -8,7 +13,7 @@ const HomePage = () => {
       <WrapperTypeProduct>
       {arr.map((item)=>{return <TypeProduct key={item} name={item}/>})}
       </WrapperTypeProduct>
-      HomePage
+      <SliderComponent arrImages = {[slider1,slider2,slider3,slider4]}/>
     </div>
     
   )
